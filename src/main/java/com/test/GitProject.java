8cc836428a5dae4043a1080e652f92d51b9f8845
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 20:00:56 +0530

    first push

---
+++ src/main/java/com/test/GitProject.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class GitProject {
+
+	public void method1() {
+System.out.println("method1");
+	}
+	public void method2() {
+System.out.println("method1");
+	}
+	public void method3() {
+System.out.println("method1");
+	}
+}
